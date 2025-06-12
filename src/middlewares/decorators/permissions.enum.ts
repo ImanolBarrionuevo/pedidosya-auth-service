50b71a9c608f5f6ebef37a97d97077e60caaca6b
MateoBroilo <broilomateo@gmail.com>
2025-06-12 14:13:13 -0300

    Agregué permisos en enum

--- src/middlewares/decorators/permissions.enum.ts
+++ src/middlewares/decorators/permissions.enum.ts
@@ -2,6 +2,15 @@ export enum Permissions {
   CreateUser = 'CREATE_USER',
   ModifyUser = 'MODIFY_USER',
   ReadUser = 'READ_USER',
-  DeleteUser = 'DELETE_USER'
-  //Seguir agregando permisos
+  DeleteUser = 'DELETE_USER',
+
+  CreateRole = 'CREATE_ROLE',
+  ModifyRole = 'MODIFY_ROLE',
+  ReadRole = 'READ_ROLE',
+  DeleteRole = 'DELETE_ROLE',
+
+  CreatePermision = 'CREATE_PERMISION',
+  ModifyPermision = 'MODIFY_PERMISION',
+  ReadPermision = 'READ_PERMISION',
+  DeletePermision = 'DELETE_PERMISION'
 }
