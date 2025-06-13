22d33651370a2124bb1304c3a35e67ec18e86593
ImanolBarrionuevo <barrionuevoimanol@gmail.com>
2025-06-13 01:28:51 -0300

    Eliminación comentario inecesario

--- src/permissions/dto/patch-permission.dto.ts
+++ src/permissions/dto/patch-permission.dto.ts
@@ -1,7 +1,7 @@
-import { IsString } from "class-validator"; //Eliminar importaciones que no se utilicen
+import { IsString } from "class-validator";
 
 export class UpdatePermissionDto {
-    
+
     @IsString()
     code: string
 
