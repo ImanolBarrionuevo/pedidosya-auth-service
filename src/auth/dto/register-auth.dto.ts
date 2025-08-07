e87c12c219c89be2f40252cc5d5643799d88870b
Tomas Gambino <tomigambino21@gmail.com>
2025-08-07 01:05:25 -0300

    Aclaración sobre role en register-auth.dto.ts

--- src/auth/dto/register-auth.dto.ts
+++ src/auth/dto/register-auth.dto.ts
@@ -6,6 +6,7 @@ export class RegisterAuthDto extends PartialType(LoginAuthDto) {
     @IsNotEmpty()
     name: string;
 
-    @IsNotEmpty()
+    // Este rol no sera utilizado en el registro ya que siempre sera invitado (rol=1), pero se mantiene para la consistencia del DTO
+    // y facilitar la creación de usuarios con otros roles para pruebas de permisos.
     role: number = 1
 }
