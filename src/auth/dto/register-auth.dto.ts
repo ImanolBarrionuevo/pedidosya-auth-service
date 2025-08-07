ac16551b2bdafb08f27d9c8c7a255379352f1d4a
Tomas Gambino <tomigambino21@gmail.com>
2025-08-07 00:58:01 -0300

    Corrección de role en register-auth.dto.ts

--- src/auth/dto/register-auth.dto.ts
+++ src/auth/dto/register-auth.dto.ts
@@ -1,13 +1,11 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { LoginAuthDto } from "./login-auth.dto";
 import { IsNotEmpty } from "class-validator";
-import { Transform } from "class-transformer";
 
 export class RegisterAuthDto extends PartialType(LoginAuthDto) {
     @IsNotEmpty()
     name: string;
 
     @IsNotEmpty()
-    @Transform(({ value }) => ({ id: value }))
     role: number = 1
 }
